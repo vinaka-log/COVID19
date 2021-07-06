fb941c430d4067d745ece9eec68c309b696ce374
vinaka <k64.yuta29@gmail.com>
2021-07-07 00:09:16 +0900

    RadarPlot

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import LinePlot from './components/lLinePlot';
 import PiePlot from './components/PiePlot';
 import BarPlot from './components/BarPlot';
+import RadarPlot from './components/RadarPlot';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <LinePlot />
       <PiePlot/>
       <BarPlot />
+      <RadarPlot />
     </div>
   );
 }
