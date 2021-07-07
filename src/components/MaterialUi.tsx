05347366e7b9b4b3339862e06e4247e1ed6d85c8
vinaka <k64.yuta29@gmail.com>
2021-07-07 23:00:15 +0900

    typography

--- src/components/MaterialUi.tsx
+++ src/components/MaterialUi.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import {Button, makeStyles} from "@material-ui/core"
+import {Button, makeStyles, Typography} from "@material-ui/core"
+import { blue } from '@material-ui/core/colors';
 
 const useStyles = makeStyles({
     btnStyle: {
         background: "green",
         padding: "3px 50px",
     },
+    typoStyle: {
+        color: "blue",
+    }
 });
 
 const MaterialUi: React.FC = () => {
@@ -13,6 +17,7 @@ const MaterialUi: React.FC = () => {
 
     return (
         <div>
+            <Typography  className={classes.typoStyle} color="secondary" variant="h1" align="right" gutterBottom noWrap>Hello UI Hello UI Hello UI Hello UI Hello UI Hello UI</Typography>
              <Button className={classes.btnStyle} variant= "contained" color="primary">
                  Button
             </Button>
