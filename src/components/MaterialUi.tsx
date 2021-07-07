88a548868f2a8b8968406bf3bb130335ed42b3ae
vinaka <k64.yuta29@gmail.com>
2021-07-07 23:17:19 +0900

    Grid

--- src/components/MaterialUi.tsx
+++ src/components/MaterialUi.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Button, makeStyles, Typography} from "@material-ui/core"
+import {Button, makeStyles, Typography, Grid, Paper} from "@material-ui/core"
 import { blue } from '@material-ui/core/colors';
 
 const useStyles = makeStyles({
@@ -9,7 +9,11 @@ const useStyles = makeStyles({
     },
     typoStyle: {
         color: "blue",
-    }
+    },
+    paperStyle:{
+        background: "orange",
+        height: "50px"
+    } 
 });
 
 const MaterialUi: React.FC = () => {
@@ -18,6 +22,29 @@ const MaterialUi: React.FC = () => {
     return (
         <div>
             <Typography  className={classes.typoStyle} color="secondary" variant="h1" align="right" gutterBottom noWrap>Hello UI Hello UI Hello UI Hello UI Hello UI Hello UI</Typography>
+            <Grid container spacing={1}>
+                <Grid item xs={12}>
+                    <Paper className={classes.paperStyle}>xs=10</Paper>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <Paper className={classes.paperStyle}>xs=10</Paper>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <Paper className={classes.paperStyle}>xs=10</Paper>
+                </Grid>
+                <Grid item xs={6} md={3}>
+                    <Paper className={classes.paperStyle}>xs=10</Paper>
+                </Grid>
+                <Grid item xs={6} md={3}>
+                    <Paper className={classes.paperStyle}>xs=10</Paper>
+                </Grid>
+                <Grid item xs={6} md={3}>
+                    <Paper className={classes.paperStyle}>xs=10</Paper>
+                </Grid>
+                <Grid item xs={6} md={3}>
+                    <Paper className={classes.paperStyle}>xs=10</Paper>
+                </Grid>
+            </Grid>
              <Button className={classes.btnStyle} variant= "contained" color="primary">
                  Button
             </Button>
