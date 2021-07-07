2f45a8479c606b262e62bc59c29fe5d166504bd5
vinaka <k64.yuta29@gmail.com>
2021-07-07 22:46:35 +0900

    makeStyles

--- src/components/MaterialUi.tsx
+++ src/components/MaterialUi.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import {Button} from "@material-ui/core"
+import {Button, makeStyles} from "@material-ui/core"
+
+const useStyles = makeStyles({
+    btnStyle: {
+        background: "green",
+        padding: "3px 50px",
+    },
+});
 
 const MaterialUi: React.FC = () => {
+    const classes = useStyles();
+
     return (
         <div>
-             <Button variant= "contained" color="secondary">
+             <Button className={classes.btnStyle} variant= "contained" color="primary">
                  Button
             </Button>
         </div>
